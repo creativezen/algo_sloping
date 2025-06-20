f7994938daaf085d1c874b5684548514b7e1c48c
creativezen <tocha2782@yandex.ru>
2025-06-20 23:41:52 +0300

    Добавлено создание сессии и загрузка конфигурации БД

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 import asyncio
 import configparser
+from loguru import logger
 
 import db
 import sloping
 
+conf_db: db.ConfigInfo
 
 async def main():
-    pass
+    global session
+    global conf_db
+    
+    session = await db.connect(
+        config['DB']['host'],
+        int(config['DB']['port']),
+        config['DB']['user'],
+        config['DB']['password'],
+        config['DB']['db'],
+    )
+    
+    conf_db = await db.load_config()
+    logger.info(f"config_db: {conf_db.__dict__}")
 
 
 if __name__ == "__main__":
+    config = configparser.ConfigParser()
+    config.read("config.ini")
     asyncio.run(main())
