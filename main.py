dfeba84eacd1b52e50bd3bf8e5753c646f6f5ef8
creativezen <tocha2782@yandex.ru>
2025-06-23 15:11:40 +0300

    Инициализация BybitClient

--- main.py
+++ main.py
@@ -3,20 +3,39 @@ import configparser
 from loguru import logger
 
 import db
+from bybit.client import BybitClient
 import sloping
 
+
+# Получаем конфигурацию из файла config.ini
+config = configparser.ConfigParser()
+config.read("config.ini")
+
+# Переменные для работы с БД
 conf_db: db.ConfigInfo
+
+# Для работы с Bybit API
+client: BybitClient
 
 async def main():
     global session
     global conf_db
     
+    client = BybitClient(
+        api_key=config['BYBIT']['API_KEY'],
+        secret_key=config['BYBIT']['SECRET_KEY'],
+        testnet=config['BYBIT']['TESTNET'],
+        api_url=config['BYBIT']['BASE_URL'] if config['BYBIT']['TESTNET'] else config['BYBIT']['TESNET_URL'],
+        category=config['BYBIT']['CATEGORY'],
+        recv_window=config['BYBIT']['RECV_WINDOW'],
+    )
+    
     session = await db.connect(
-        config['DB']['host'],
-        int(config['DB']['port']),
-        config['DB']['user'],
-        config['DB']['password'],
-        config['DB']['db'],
+        config['DB']['HOST'],
+        int(config['DB']['PORT']),
+        config['DB']['USER'],
+        config['DB']['PASSWORD'],
+        config['DB']['DB'],
     )
     
     conf_db = await db.load_config()
@@ -24,6 +43,4 @@ async def main():
 
 
 if __name__ == "__main__":
-    config = configparser.ConfigParser()
-    config.read("config.ini")
     asyncio.run(main())
